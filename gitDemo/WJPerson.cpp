25d2652586327dbef0e5db4c080139fc9393689c
takjay19 <Jay_ccccccccc@chanWenJaydeMacBook-Pro.local>
2015-05-31 23:02:16 +0800

    Add C++ file

---
+++ gitDemo/WJPerson.cpp
@@ -0,0 +1,14 @@
+//
+//  WJPerson.cpp
+//  gitDemo
+//
+//  Created by chanWenJay on 15/5/31.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#include "WJPerson.h"
+
+void Person::sayHello() {
+    printf("Hello Bafeng");
+}
+
---
+++ gitDemo/WJPerson.h
@@ -0,0 +1,21 @@
+//
+//  WJPerson.h
+//  gitDemo
+//
+//  Created by chanWenJay on 15/5/31.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#ifndef __gitDemo__WJPerson__
+#define __gitDemo__WJPerson__
+
+#include <stdio.h>
+
+class Person {
+    
+    
+public:
+    void sayHello();
+};
+
+#endif /* defined(__gitDemo__WJPerson__) */
