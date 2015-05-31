74f348da695218330e89192e9be384e4381717d5
张炯枫 <fisland@fisland-MacBook-pro.local>
2015-05-31 23:15:25 +0800

    viewdisload

---
+++ gitDemo/JFView.h
@@ -0,0 +1,13 @@
+//
+//  JFView.h
+//  gitDemo
+//
+//  Created by 张炯枫 on 15/5/31.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFView : UIView
+
+@end
---
+++ gitDemo/JFView.m
@@ -0,0 +1,21 @@
+//
+//  JFView.m
+//  gitDemo
+//
+//  Created by 张炯枫 on 15/5/31.
+//  Copyright (c) 2015年 fisland. All rights reserved.
+//
+
+#import "JFView.h"
+
+@implementation JFView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
